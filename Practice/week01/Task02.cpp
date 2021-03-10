80efee7e9810f121c6e77557cb64fdb11f701396
ElizabethVK <beeetiiis@gmail.com>
2021-03-10 15:10:48 +0200

    task02

--- Practice/week01/Task02.cpp
+++ Practice/week01/Task02.cpp
@@ -24,13 +24,46 @@ struct Point{
         return sqrt(x*x+y*y);
     }
 
-    double distanceTwoPoints(){
-        double 
-    }
+    
 
 };
 
+double distanceTwoPoints(Point pt1, Point pt2){
+    double biggieX = ((pt1.x > pt2.x) ? pt1.x : pt2.x);
+    double smolX = ((pt1.x < pt2.x) ? pt1.x : pt2.x);
+
+    double biggieY = ((pt1.y > pt2.y) ? pt1.y : pt2.y);
+    double smolY = ((pt1.y < pt2.y) ? pt1.y : pt2.y);
+
+    return sqrt() 
+}
 
 int main(){
 
+    Point pt1, pt2;
+
+    cout << "Pt1 stuff: " << endl;
+
+    pt1.readCoordinates();
+
+    cout << "Pt2 stuff: " << endl;
+
+    pt2.readCoordinates();
+
+    pt1.printCoordinates();
+
+    pt2.printCoordinates();
+
+    double l1 =0, l2=0;
+
+    l1 = pt1.distanceStart();
+    l2 = pt2.distanceStart();
+
+    cout << l1 << endl << l2 << endl;
+
+    double distance =0;
+
+    distance = 
+
+    return 0;
 }
