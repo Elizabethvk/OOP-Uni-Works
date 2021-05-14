be6bc7c8ae752f71a7945213bb3886b3603c1772
elizabethvk <elizabeetvk@gmail.com>
2021-05-14 13:55:22 +0300

    w10 t03

---
+++ Practice/week10/Task03/main.cpp
@@ -0,0 +1,34 @@
+#include "space.hpp"
+#include <cstring>
+
+int main() {
+    NASA cheti;
+    cheti.readFile("Leti.txt");
+    cheti.print();
+
+    cout << "---------" << endl;
+
+    cheti.saveBinary("BinarnoLet.txt");
+
+    struct person {
+        char name[30];
+        int age;
+    };
+
+    person p;
+    strcpy(p.name, "Some Name");
+
+    p.age = 10;
+    
+    std::ofstream file;
+    file.open("BinaryTest.txt");
+
+    file.write((char*)& p, sizeof(person));
+
+    //NASA binChet;
+    
+    //binChet.saveBinary("BinarnoLetim.bin");
+    //binChet.print();
+
+    return 0;
+}
